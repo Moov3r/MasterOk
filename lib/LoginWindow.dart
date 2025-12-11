eee847e2473adb79525e904cdd43bc725a37ee73
Prohor <prohorsakun6@gmail.com>
2025-12-12 01:04:46 +0700

    Create file LoginWindow

---
+++ lib/LoginWindow.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class LoginWindow extends StatelessWidget {
+  const LoginWindow({super.key});
+  
+  @override
+  Widget build(BuildContext context) => throw UnimplementedError();
+}
